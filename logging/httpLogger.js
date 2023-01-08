1029899d6f590620548c09a262101631225c569f
Moyinoluwa-10 <moyinadelowo@gmail.com>
2023-01-08 02:25:25 +0100

    feat: properly setup loggers

--- logging/httpLogger.js
+++ logging/httpLogger.js
@@ -1,36 +1,32 @@
-const morgan = require('morgan')
-const json = require('morgan-json')
-const logger = require('./logger')
+const morgan = require("morgan");
+const json = require("morgan-json");
+const logger = require("./logger");
 
 const format = json({
-  method: ':method',
-  url: ':url',
-  status: ':status',
-  contentLength: ':res[content-length]',
-  responseTime: ':response-time'
-})
+  method: ":method",
+  url: ":url",
+  status: ":status",
+  contentLength: ":res[content-length]",
+  responseTime: ":response-time",
+});
 
 const httpLogger = morgan(format, {
   stream: {
     write: (message) => {
-      const {
-        method,
-        url,
-        status,
-        contentLength,
-        responseTime
-      } = JSON.parse(message)
+      const { method, url, status, contentLength, responseTime } =
+        JSON.parse(message);
 
-      logger.info('HTTP Access Log', {
+      logger.info("HTTP Access Log", {
         timestamp: new Date().toString(),
         method,
         url,
         status: Number(status),
         contentLength,
-        responseTime: Number(responseTime)
-      })
-    }
-  }
-})
+        responseTime: Number(responseTime),
+      });
+    },
+  },
+});
 
-module.exports = httpLogger
+module.exports = httpLogger;
+
